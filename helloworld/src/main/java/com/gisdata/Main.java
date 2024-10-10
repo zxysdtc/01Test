f1d458a6a6bf71ed4b4369c6fd9b488bf09b5eb2
zxysdtc <zxysdtc321@gmail.com>
2024-10-10 16:57:12 +0800

    branch

--- helloworld/src/main/java/com/gisdata/Main.java
+++ helloworld/src/main/java/com/gisdata/Main.java
@@ -1,10 +1,11 @@
-package src.main.java.com.gisdata;
+package com.gisdata;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello World!");
         System.out.println("hhhh");
         System.out.println("hhhh");
+
     }
 
 }
