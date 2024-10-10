c8d3386c6d6deaed93ff3ae8cc3e4ad1b1a3a3f3
zxysdtc <zxysdtc321@gmail.com>
2024-10-10 16:47:02 +0800

    2change

--- helloworld/src/main/java/com/gisdata/Main.java
+++ helloworld/src/main/java/com/gisdata/Main.java
@@ -1,8 +1,9 @@
-package com.gisdata;
+package src.main.java.com.gisdata;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("hhhh");
         System.out.println("hhhh");
     }
 
